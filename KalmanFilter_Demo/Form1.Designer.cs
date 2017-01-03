de9102980f77d0d7a8682023f846b7c5d1951f64
unknown <>
2017-01-03 21:22:55 +0800

    add code

--- KalmanFilter_Demo/Form1.Designer.cs
+++ KalmanFilter_Demo/Form1.Designer.cs
@@ -28,12 +28,95 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.产生随机数列ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.产生随机数列ToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(881, 28);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // 产生随机数列ToolStripMenuItem
+            // 
+            this.产生随机数列ToolStripMenuItem.Name = "产生随机数列ToolStripMenuItem";
+            this.产生随机数列ToolStripMenuItem.Size = new System.Drawing.Size(111, 24);
+            this.产生随机数列ToolStripMenuItem.Text = "产生随机数列";
+            this.产生随机数列ToolStripMenuItem.Click += new System.EventHandler(this.产生随机数列ToolStripMenuItem_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 106);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(142, 302);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "";
+            // 
+            // chart1
+            // 
+            chartArea2.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea2);
+            legend2.Name = "Legend1";
+            this.chart1.Legends.Add(legend2);
+            this.chart1.Location = new System.Drawing.Point(371, 31);
+            this.chart1.Name = "chart1";
+            series2.ChartArea = "ChartArea1";
+            series2.Legend = "Legend1";
+            series2.Name = "Series1";
+            this.chart1.Series.Add(series2);
+            this.chart1.Size = new System.Drawing.Size(498, 419);
+            this.chart1.TabIndex = 2;
+            this.chart1.Text = "chart1";
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Location = new System.Drawing.Point(169, 106);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(154, 302);
+            this.richTextBox2.TabIndex = 3;
+            this.richTextBox2.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(881, 509);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem 产生随机数列ToolStripMenuItem;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.RichTextBox richTextBox2;
     }
 }
 
--- KalmanFilter_Demo/Form1.cs
+++ KalmanFilter_Demo/Form1.cs
@@ -7,14 +7,105 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
 
 namespace KalmanFilter_Demo
 {
     public partial class Form1 : Form
     {
+        double[] CanShu = { 23, 9, 16, 16, 1, 0, 0, 0 };
+
+        //double[] Observ = { 22, 24, 24, 25, 24, 26, 21, 26, };
+
+        //double[] Observ = { 25, 26 };
+
+        double[] ObsRand = new double[100];
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void 产生随机数列ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            //产生随机数列
+            for (int i = 0; i < 100; i++)
+            {
+                //Random Random1 = new Random();//这样声明的话，如果多次调用rnd则会会出现每次的随机数值都一样
+                Random Random1 = new Random((int)DateTime.Now.Ticks);   //利用系统日期作为参数传进去之后可以解决上面出现的问题
+                double i1 = Random1.Next(20, 30);
+                double result = Random1.NextDouble() * (-0.99) + 0.99;//随机生成-0.90至0.99的随机双精度数值
+                ObsRand[i] = i1 + result;
+                this.richTextBox2.Text += ObsRand[i].ToString() + "\n";
+            }
+            chart1.Series.Clear();
+            graphPoint(chart1, "测量值", ObsRand, Color.PowderBlue);
+            double[] True = GetKalMan(CanShu, ObsRand);
+
+            graph(chart1, "滤波后值", True, Color.Black);
+
+            double[] Ave = new double[ObsRand.Length];
+
+            for (int i = 0; i < ObsRand.Length; i++)
+            {
+                Ave[i] = Average;
+            }
+
+            graph(chart1, "平均值", Ave, Color.Red);
+        }
+
+        //画图函数
+        protected void graph(Chart c, string name, double[] vals, Color clr)
+        {
+            Series s = new Series(name);
+            s.ChartType = SeriesChartType.Line;
+            for (int i = 0; i < vals.Length; i++)
+            {
+                s.Points.Add(new DataPoint(i, vals[i]));
+            }
+            s.Color = clr;
+            c.Series.Add(s);
+        }
+
+        //画图函数2
+        protected void graphPoint(Chart c, string name, double[] vals, Color clr)
+        {
+            Series s = new Series(name);
+            s.ChartType = SeriesChartType.Point;
+            for (int i = 0; i < vals.Length; i++)
+            {
+                s.Points.Add(new DataPoint(i, vals[i]));
+            }
+            s.Color = clr;
+            c.Series.Add(s);
+        }
+        //滤波函数
+
+        double Average;
+        public double[] GetKalMan(double[] CanShu, double[] Observe)
+        {
+            double KamanX = CanShu[0];
+            double KamanP = CanShu[1];
+            double KamanQ = CanShu[2];
+            double KamanR = CanShu[3];
+            double KamanY = CanShu[4];
+            double KamanKg = CanShu[5];
+            double KamanSum = CanShu[6];
+            double[] True = new double[ObsRand.Length];
+            for (int i = 0; i <= ObsRand.Length - 1; i++)
+            {
+                KamanY = KamanX;
+                KamanP = KamanP + KamanQ;
+                KamanKg = KamanP / (KamanP + KamanR);
+                KamanX = (KamanY + KamanKg * (Observe[i] - KamanY));
+                KamanSum += KamanX;
+                True[i] = KamanX;
+                this.richTextBox1.Text += KamanX.ToString() + "\n";
+                KamanP = (1 - KamanKg) * KamanP;
+            }
+            Average = KamanSum / Observe.Length;
+            return True;
+
         }
     }
 }
